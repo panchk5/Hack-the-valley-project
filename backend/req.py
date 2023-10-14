1784de87b111feda8a6abc32c0bfd44b05f845d2
Savvy <savvy.liu@Hotmail.com>
2023-10-14 01:47:24 -0400

    can hold conversation now

--- backend/req.py
+++ backend/req.py
@@ -1,15 +1,24 @@
 import openai
 
-openai.api_key = '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' # put in env later
+openai.api_key = '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' # put in env later
+language = "python3" # make way to choose language 
+messages = [ {"role": "system", "content": "You are a helpful assistant who analyses code and provides meaningful feedback, the launguage used is" + language} ] 
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant who analyses code and provides meaningful feedback"}, 
-        {"role": "user", "content": "print(hello world)"},
-        # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        # {"role": "user", "content": "Where was it played?"}
-    ]
-)
-response_message = response["choices"][0]["message"]
-print(response_message)
+while True:
+    # ask user what language they're using
+    message = input("Enter code to analyze: ") 
+    if message == "-1": # exit 
+        break
+    if message: 
+        messages.append( 
+            {"role": "user", "content": message}, 
+        ) 
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", max_tokens = 60, messages=messages
+        ) 
+    reply = chat.choices[0].message.content 
+    print(reply) 
+    messages.append({"role": "assistant", "content": reply}) 
+
+    
+
