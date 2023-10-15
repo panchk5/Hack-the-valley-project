bd64dea92c34c316d3beb30c892e1d043e9b1dfc
Savvy <savvy.liu@Hotmail.com>
2023-10-15 00:44:29 -0400

    update openai api key

--- backend/oai.py
+++ backend/oai.py
@@ -1,15 +1,16 @@
+"""OpenAI API connector."""
 
 # Import from standard library
-import os
 import logging
 
 # Import from 3rd party libraries
 import openai
-
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 # Assign credentials from environment variable or streamlit secrets dict
-openai.api_key = "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"
+openai.api_key = os.environ.get("API_KEY_1")
 
 # Suppress openai request/response logging
 # Handle by manually changing the respective APIRequestor methods in the openai package
@@ -36,7 +37,7 @@ class Openai:
             logging.error(f"OpenAI API error: {e}")
 
     @staticmethod
-    def complete(prompt: str, temperature: float = 0.5, max_tokens: int = 1000) -> str:
+    def complete(prompt: str, temperature: float = 0.9, max_tokens: int = 1000) -> str:
         """Call OpenAI GPT Completion with text prompt.
         Args:
             prompt: text prompt
@@ -47,29 +48,13 @@ class Openai:
             "prompt": prompt,
             "temperature": temperature,
             "max_tokens": max_tokens,
+            "top_p": 1,  # default
+            "frequency_penalty": 0,  # default,
+            "presence_penalty": 0,  # default
         }
         try:
             response = openai.Completion.create(**kwargs)
             return response["choices"][0]["text"]
-
-        except Exception as e:
-            logging.error(f"OpenAI API error: {e}")
-
-    @staticmethod
-    def image(prompt: str) -> str:
-        """Call OpenAI Image Create with text prompt.
-        Args:
-            prompt: text prompt
-        Return: image url
-        """
-        try:
-            response = openai.Image.create(
-                prompt=prompt,
-                n=1,
-                size="512x512",
-                response_format="url",
-            )
-            return response["data"][0]["url"]
 
         except Exception as e:
             logging.error(f"OpenAI API error: {e}")
